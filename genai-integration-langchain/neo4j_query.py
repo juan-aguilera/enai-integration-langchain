a28ca0f92cc26993df0f64d66569af83db7514a7
juan-aguilera <158538464+juan-aguilera@users.noreply.github.com>
2025-11-17 21:49:28 -0500

    Update neo4j_query.py

--- genai-integration-langchain/neo4j_query.py
+++ genai-integration-langchain/neo4j_query.py
@@ -1,7 +1,19 @@
 import os
 from dotenv import load_dotenv
+from langchain_neo4j import Neo4jGraph
 load_dotenv()
 
 # Create Neo4jGraph instance
+graph = Neo4jGraph(
+    url=os.getenv("NEO4J_URI"),
+    username=os.getenv("NEO4J_USERNAME"), 
+    password=os.getenv("NEO4J_PASSWORD"),
+)
 
 # Run a query and print the result
+result = graph.query("""
+MATCH (m:Movie {title: "Mission: Impossible"})<-[a:ACTED_IN]-(p:Person)
+RETURN p.name AS actor, a.role AS role
+""")
+
+print(result)
