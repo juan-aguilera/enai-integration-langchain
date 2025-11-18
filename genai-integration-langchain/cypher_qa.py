ff16f501799fe5a43248b35413e2f94fc72e41c5
juan-aguilera <158538464+juan-aguilera@users.noreply.github.com>
2025-11-17 23:40:26 -0500

    Update cypher_qa.py

--- genai-integration-langchain/cypher_qa.py
+++ genai-integration-langchain/cypher_qa.py
@@ -5,6 +5,7 @@ load_dotenv()
 from langchain.chat_models import init_chat_model
 from langchain_neo4j import Neo4jGraph
 from langchain_neo4j import GraphCypherQAChain
+from langchain_core.prompts.prompt import PromptTemplate
 
 # Initialize the LLM
 model = init_chat_model(
@@ -25,12 +26,45 @@ graph = Neo4jGraph(
     username=os.getenv("NEO4J_USERNAME"), 
     password=os.getenv("NEO4J_PASSWORD"),
 )
+# Cypher template
+# Cypher template with examples
+cypher_template = """Task:Generate Cypher statement to query a graph database.
+Instructions:
+Use only the provided relationship types and properties in the schema.
+Do not use any other relationship types or properties that are not provided.
+For movie titles that begin with "The", move "the" to the end, for example "The 39 Steps" becomes "39 Steps, The".
 
+Schema:
+{schema}
+Examples:
+1. Question: Get user ratings?
+   Cypher: MATCH (u:User)-[r:RATED]->(m:Movie) WHERE u.name = "User name" RETURN r.rating AS userRating
+2. Question: Get average rating for a movie?
+   Cypher: MATCH (m:Movie)<-[r:RATED]-(u:User) WHERE m.title = 'Movie Title' RETURN avg(r.rating) AS userRating
+3. Question: Get movies for a genre?
+   Cypher: MATCH ((m:Movie)-[:IN_GENRE]->(g:Genre) WHERE g.name = 'Genre Name' RETURN m.title AS movieTitle
+
+Note: Do not include any explanations or apologies in your responses.
+Do not respond to any questions that might ask anything else than for you to construct a Cypher statement.
+Do not include any text except the generated Cypher statement.
+
+The question is:
+{question}"""
+
+cypher_prompt = PromptTemplate(
+    input_variables=["schema", "question"], 
+    template=cypher_template
+)
 # Create the Cypher QA chain
 cypher_qa = GraphCypherQAChain.from_llm(
     graph=graph, 
     llm=model, 
     cypher_llm=cypher_model,
+    cypher_prompt=cypher_prompt,
+    #Schema - If you wanted to just include data about movies and their directors
+    include_types=["Movie", "ACTED_IN", "Person"],
+    #Schema - if you wanted to exclude ratings data, you could provide User and RATED as the types to the exclude_types
+    exclude_types=["User", "RATED"],
     allow_dangerous_requests=True,
     verbose=True, 
 )
