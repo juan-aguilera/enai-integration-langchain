12e4f1df4850d37d2a7255320137daa865364641
juan-aguilera <158538464+juan-aguilera@users.noreply.github.com>
2025-11-17 23:50:11 -0500

    Update cypher_retriever.py

--- genai-integration-langchain/cypher_retriever.py
+++ genai-integration-langchain/cypher_retriever.py
@@ -8,6 +8,7 @@ from langgraph.graph import START, StateGraph
 from langchain_core.prompts import PromptTemplate
 from typing_extensions import List, TypedDict
 from langchain_neo4j import Neo4jGraph
+from langchain_neo4j import GraphCypherQAChain
 
 # Initialize the LLM
 model = init_chat_model("gpt-4o", model_provider="openai")
@@ -38,15 +39,23 @@ graph = Neo4jGraph(
 )
 
 # Create the Cypher QA chain
-# cypher_qa = 
-
+cypher_qa = GraphCypherQAChain.from_llm(
+    graph=graph, 
+    llm=model, 
+    allow_dangerous_requests=True,
+    return_direct=True,
+) 
+"""
+The return_direct parameter is set to True to return the result of the Cypher query instead of an answer.
+This is useful when you want to pass the raw data to the agent for further processing or analysis.
+"""
 # Define functions for each step in the application
 
 # Retrieve context 
 def retrieve(state: State):
-    context = [
-        {"data": "None"}
-    ]
+    context = cypher_qa.invoke(
+        {"query": state["question"]}
+    )
     return {"context": context}
 
 # Generate the answer based on the question and context
@@ -66,4 +75,23 @@ response = app.invoke({"question": question})
 print("Answer:", response["answer"])
 print("Context:", response["context"])
 
+"""
+Improve the retriever
+Your challenge is to improve the retriever using the techniques you learned in the previous lessons, which could include:
+
+Providing a custom prompt and specific instructions.
+Including example questions and Cypher queries.
+Using a different LLM model for Cypher generation.
+Restricting the schema to provide more focused results.
+
+Here are some examples of more complex questions you can try:
+
+When was the movie The Abyss released?
+What is the highest grossing movie of all time?
+Can you recommend a Horror movie based on user rating?
+What movies scored about 4 for user rating?
+What are the highest rated movies with more than 100 ratings?
+
+There is no right or wrong solution. You should experiment with different approaches to see how they affect the accuracy and relevance of the generated Cypher queries.
+"""
 
